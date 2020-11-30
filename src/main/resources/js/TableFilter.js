5d8f34f9b514b43d9f94aa59421fe7ab51de1990
Sam Murphy <stmurphy@agiledigital.com.au>
2020-11-30 18:03:39 +1100

    fix(ADE-495): Move some arbitrary values to const. Code formatting

--- src/main/resources/js/TableFilter.js
+++ src/main/resources/js/TableFilter.js
@@ -1,15 +1,17 @@
-const $ = AJS.$
+const $ = AJS.$;
+
+const searchIdPrefix = "search-";
 
 const appConstants = {
-  TECHNOLOGIES_LIST: 'technologies-list',
-  TECHNOLOGIES_SEARCH: 'search-technologies',
-  TECHNOLOGIES_COLUMN_CLASS: 'cell-technology',
-  TECHNOLOGIES_COLUMN_KEY: 'technologies',
-  STATUS_LIST: 'status-list',
-  STATUS_SEARCH: 'search-status',
-  STATUS_COLUMN_CLASS: 'cell-status',
-  STATUS_COLUMN_KEY: 'status',
-}
+  TECHNOLOGIES_LIST: "technologies-list",
+  TECHNOLOGIES_SEARCH: `${searchIdPrefix}technologies`,
+  TECHNOLOGIES_COLUMN_CLASS: "cell-technology",
+  TECHNOLOGIES_COLUMN_KEY: "technologies",
+  STATUS_LIST: "status-list",
+  STATUS_SEARCH: `${searchIdPrefix}status`,
+  STATUS_COLUMN_CLASS: "cell-status",
+  STATUS_COLUMN_KEY: "status",
+};
 
 /**
  * Status of each column per row
@@ -24,7 +26,7 @@ let rowStatus;
  */
 let itemLists = {
   [appConstants.TECHNOLOGIES_LIST]: new Set(),
-  [appConstants.STATUS_LIST]: new Set()
+  [appConstants.STATUS_LIST]: new Set(),
 };
 
 /**
@@ -33,7 +35,7 @@ let itemLists = {
  */
 let usedOptions = {
   [appConstants.TECHNOLOGIES_LIST]: [],
-  [appConstants.STATUS_LIST]: []
+  [appConstants.STATUS_LIST]: [],
 };
 
 /**
@@ -43,22 +45,23 @@ let usedOptions = {
 let multiSelectFocus = {
   [appConstants.TECHNOLOGIES_LIST]: {
     container: false,
-    list: false
+    list: false,
   },
   [appConstants.STATUS_LIST]: {
     container: false,
-    list: false
-  }
-}
+    list: false,
+  },
+};
 
 /**
  * Translate a column class to a column key
  * @type {{[p: string]: string}}
  */
 const columnClassToColumnKey = {
-  [appConstants.TECHNOLOGIES_COLUMN_CLASS]: appConstants.TECHNOLOGIES_COLUMN_KEY,
-  [appConstants.STATUS_COLUMN_CLASS]: appConstants.STATUS_COLUMN_KEY
-}
+  [appConstants.TECHNOLOGIES_COLUMN_CLASS]:
+    appConstants.TECHNOLOGIES_COLUMN_KEY,
+  [appConstants.STATUS_COLUMN_CLASS]: appConstants.STATUS_COLUMN_KEY,
+};
 
 /**
  * Translate a list id to a column key
@@ -67,7 +70,7 @@ const columnClassToColumnKey = {
 const listToColumnClass = {
   [appConstants.TECHNOLOGIES_LIST]: appConstants.TECHNOLOGIES_COLUMN_CLASS,
   [appConstants.STATUS_LIST]: appConstants.STATUS_COLUMN_CLASS,
-}
+};
 
 /**
  * Translate a input id to a list id
@@ -75,7 +78,7 @@ const listToColumnClass = {
  */
 const searchToList = {
   [appConstants.TECHNOLOGIES_SEARCH]: appConstants.TECHNOLOGIES_LIST,
-  [appConstants.STATUS_SEARCH]: appConstants.STATUS_LIST
+  [appConstants.STATUS_SEARCH]: appConstants.STATUS_LIST,
 };
 
 /**
@@ -85,26 +88,34 @@ const searchToList = {
 const listToSearch = {
   [appConstants.TECHNOLOGIES_LIST]: appConstants.TECHNOLOGIES_SEARCH,
   [appConstants.STATUS_LIST]: appConstants.STATUS_SEARCH,
-}
+};
+
+// Script references
+const closeSymbol = '&#x2715';
+const enterKeyCode = 13;
 
 /**
  * Determines the visability of each row from the RowColumnStauts container
  */
 const setHidden = () => {
-  $('.table-content tr').each((index, element) => {
-    $(element)[Object.values(rowStatus[index]).some((value) => !value)
-        ? 'addClass'
-        : 'removeClass']('hidden');
-  })
-}
+  $(".table-content tr").each((index, element) => {
+    $(element)[
+      Object.values(rowStatus[index]).some((value) => !value)
+        ? "addClass"
+        : "removeClass"
+    ]("hidden");
+  });
+};
 
 /**
  * Generates the options for a dropdown list from a list of values
  *
  * @param {Set<string>} list A set of items to add to generate list items for
  */
-const generateHTMLTagsForListItems = (list) => [...list].map(
-    (value) => `<li class="list-option" role="option">${value}</li>`);
+const generateHTMLTagsForListItems = (list) =>
+  [...list].map(
+    (value) => `<li class="list-option" role="option">${value}</li>`
+  );
 
 /**
  * Filters the dropdown list based off the statuses of each option
@@ -112,10 +123,12 @@ const generateHTMLTagsForListItems = (list) => [...list].map(
  * @param {string} listName Name of the list to modify
  */
 const changeStatusOfOptionInList = (listName) => {
-  $(`#${listName}`)[Object.values(
-      multiSelectFocus[listName]).some(
-      (value) => value) ? 'removeClass' : 'addClass']('hidden');
-}
+  $(`#${listName}`)[
+    Object.values(multiSelectFocus[listName]).some((value) => value)
+      ? "removeClass"
+      : "addClass"
+  ]("hidden");
+};
 
 /**
  * Adds a tag to the multiselect field. Hides item from the dropdown list
@@ -127,16 +140,18 @@ const changeStatusOfOptionInList = (listName) => {
 const addTagToMultiSelect = (searchField, list, element) => {
   if (element !== undefined) {
     $(`#${searchField}`).before(
-        `<span class='tag'>${$(
-            element).text()}<button type="button" class="close">&#x2715</button></span>`);
-    $(element).addClass('hidden');
+      `<span class='tag'>${$(
+        element
+      ).text()}<button type="button" class="close">${closeSymbol}</button></span>`
+    );
+    $(element).addClass("hidden");
     usedOptions[list].push(element);
     calculateHiddenRowForColumn(list);
     return true;
   }
 
   return false;
-}
+};
 
 /**
  * Removes a tag from the multiselect list. Item will show in the dropdown list
@@ -148,13 +163,15 @@ const addTagToMultiSelect = (searchField, list, element) => {
 const removeTagFromMultiSelect = (list, element) => {
   const text = $(element).text().slice(0, -1);
   const listItem = usedOptions[list].filter(
-      (value) => $(value).text() === text)[0];
-  $(listItem).removeClass('hidden');
+    (value) => $(value).text() === text
+  )[0];
+  $(listItem).removeClass("hidden");
 
   usedOptions[list] = usedOptions[list].filter(
-      (value) => $(value).text() !== text);
+    (value) => $(value).text() !== text
+  );
   calculateHiddenRowForColumn(list);
-}
+};
 
 /**
  * Sets the visability status for a item in a dropdown list
@@ -163,16 +180,20 @@ const removeTagFromMultiSelect = (list, element) => {
  * @param {string} searchValue Needle to search haystack for
  */
 const setHiddenFlagsOnDropdownList = (listName, searchValue) => {
-  const technologiesLabels = usedOptions[listName].map(
-      (value) => $(value).text());
+  const technologiesLabels = usedOptions[listName].map((value) =>
+    $(value).text()
+  );
 
   $(`#${listName} li`).each((index, value) => {
     if (!technologiesLabels.includes($(value).text())) {
-      $(value)[$(value).text().toLowerCase().includes(searchValue)
-          ? 'removeClass' : 'addClass']('hidden');
+      $(value)[
+        $(value).text().toLowerCase().includes(searchValue)
+          ? "removeClass"
+          : "addClass"
+      ]("hidden");
     }
   });
-}
+};
 
 /**
  * Checks each row of the table and sees if a column in that row fit the
@@ -181,8 +202,7 @@ const setHiddenFlagsOnDropdownList = (listName, searchValue) => {
  * @param {string} list Id of the dropdown list
  */
 const calculateHiddenRowForColumn = (list) => {
-  const optionsList = usedOptions[list].map(
-      (value) => $(value).text());
+  const optionsList = usedOptions[list].map((value) => $(value).text());
 
   const columnClass = listToColumnClass[list];
 
@@ -192,10 +212,11 @@ const calculateHiddenRowForColumn = (list) => {
     if (optionsList.length > 0) {
       const rowList = [];
 
-      $(value).children(`span.${columnClass.substring(5)}`).each(
-          (index, option) => {
-            rowList.push($(option).text());
-          });
+      $(value)
+        .children(`span.${columnClass.substring(5)}`)
+        .each((index, option) => {
+          rowList.push($(option).text());
+        });
 
       hasOptions = optionsList.map((value) => rowList.includes(value));
     } else {
@@ -203,11 +224,12 @@ const calculateHiddenRowForColumn = (list) => {
     }
 
     rowStatus[index][columnClassToColumnKey[columnClass]] = hasOptions.every(
-        (value) => value);
+      (value) => value
+    );
   });
 
   setHidden();
-}
+};
 
 /**
  * Handles the focusing of a dropdown list
@@ -216,11 +238,12 @@ const calculateHiddenRowForColumn = (list) => {
  * @param isFocus Is the element currently in focus
  */
 const handleListFocus = (element, isFocus) => {
-  const listId = element.id.includes("list") ? element.id : $(element).parent(
-      'ul').attr('id');
+  const listId = element.id.includes("list")
+    ? element.id
+    : $(element).parent("ul").attr("id");
   multiSelectFocus[listId].list = isFocus;
   changeStatusOfOptionInList(listId);
-}
+};
 
 /**
  * Handles the focusing of a search box container
@@ -229,25 +252,29 @@ const handleListFocus = (element, isFocus) => {
  * @param isFocus Is the element currently focused
  */
 const handleContainerFocus = (element, isFocus) => {
-  const listId = searchToList[
-      element.id.includes("search") ? element.id : $(element).closest(
-          'div').children('input')[0].id];
+  const listId =
+    searchToList[
+      element.id.includes("search")
+        ? element.id
+        : $(element).closest("div").children("input")[0].id
+    ];
   multiSelectFocus[listId].container = isFocus;
   changeStatusOfOptionInList(listId);
-}
+};
 
 $(document).ready(() => {
-  rowStatus = new Array($('.table-content tr').length).fill(undefined).map(
-      () => ({
-        name: true,
-        description: true,
-        technologies: true,
-        status: true,
-        talkTo: true
-      }));
+  rowStatus = new Array($(".table-content tr").length)
+    .fill(undefined)
+    .map(() => ({
+      name: true,
+      description: true,
+      technologies: true,
+      status: true,
+      talkTo: true,
+    }));
 
   ((lists) => {
-    lists.forEach(({key, listRef}) => {
+    lists.forEach(({ key, listRef }) => {
       let tempList = [];
 
       $(`span.${key}`).each((index, value) => {
@@ -257,21 +284,26 @@ $(document).ready(() => {
       tempList.sort().forEach((value) => itemLists[listRef].add(value));
 
       $(`#${listRef}`).append(
-          ...generateHTMLTagsForListItems(itemLists[listRef]));
+        ...generateHTMLTagsForListItems(itemLists[listRef])
+      );
     });
-  })([{key: 'technology', listRef: appConstants.TECHNOLOGIES_LIST},
-    {key: 'status', listRef: appConstants.STATUS_LIST}]);
+  })([
+    { key: "technology", listRef: appConstants.TECHNOLOGIES_LIST },
+    { key: "status", listRef: appConstants.STATUS_LIST },
+  ]);
 
   /*
-   * Standard text search
+   * Standard text search event handlers
    */
-  $('.text-search').on('input', ({target}) => {
+  $(".text-search").on("input", ({ target }) => {
     const searchValue = $(target).val().toLowerCase();
-    const columnClass = target.id.substring(7);
+    const columnClass = target.id.substring(searchIdPrefix.length);
 
     $(`.cell-${columnClass}`).each((index, value) => {
-      rowStatus[index][columnClass] = searchValue === '' ? true : $(
-          value).text().toLowerCase().includes(searchValue);
+      rowStatus[index][columnClass] =
+        searchValue === ""
+          ? true
+          : $(value).text().toLowerCase().includes(searchValue);
     });
     setHidden();
   });
@@ -280,44 +312,52 @@ $(document).ready(() => {
    * MultiSelect Type Ahead Event handlers
    */
   $(`.searchable`).on({
-    'input': ({target}) => {
-      setHiddenFlagsOnDropdownList(searchToList[target.id],
-          $(target).val().toLowerCase());
+    input: ({ target }) => {
+      setHiddenFlagsOnDropdownList(
+        searchToList[target.id],
+        $(target).val().toLowerCase()
+      );
     },
-    'keyup': ({keyCode, target}) => {
-      if (keyCode === 13 && $(target).val() != '') {
+    keyup: ({ keyCode, target }) => {
+      if (keyCode === enterKeyCode && $(target).val() != "") {
         const searchId = target.id;
         const listId = searchToList[searchId];
 
-        if (addTagToMultiSelect(searchId, listId,
-            $(`#${listId} li:not(.hidden):first`)[0])) {
-          $(`#${searchId}`).val('');
+        if (
+          addTagToMultiSelect(
+            searchId,
+            listId,
+            $(`#${listId} li:not(.hidden):first`)[0]
+          )
+        ) {
+          $(`#${searchId}`).val("");
           setHiddenFlagsOnDropdownList(listId, "");
         }
       }
-    }
+    },
   });
 
-  $('.multiselect-container').on({
-    'focusin': ({target}) => handleContainerFocus(target, true),
-    'focusout': ({target}) => handleContainerFocus(target, false)
+  $(".multiselect-container").on({
+    focusin: ({ target }) => handleContainerFocus(target, true),
+    focusout: ({ target }) => handleContainerFocus(target, false),
   });
 
-  $('.list-container').on({
-    'mouseenter': ({target}) => handleListFocus(target, true),
-    'mouseleave': ({target}) => handleListFocus(target, false)
+  $(".list-container").on({
+    mouseenter: ({ target }) => handleListFocus(target, true),
+    mouseleave: ({ target }) => handleListFocus(target, false),
   });
 
-  $('.list-option').on('click', ({target}) => {
-    const listId = $(target).parent().attr('id');
+  $(".list-option").on("click", ({ target }) => {
+    const listId = $(target).parent().attr("id");
     addTagToMultiSelect(listToSearch[listId], listId, target);
   });
 
-  $('.multiselect-container').on('click', 'button.close',
-      ({target}) => {
-        const parent = $(target).parent();
-        removeTagFromMultiSelect(
-            searchToList[parent.siblings('input').attr('id')], parent);
-        $(parent).remove();
-      });
+  $(".multiselect-container").on("click", "button.close", ({ target }) => {
+    const parent = $(target).parent();
+    removeTagFromMultiSelect(
+      searchToList[parent.siblings("input").attr("id")],
+      parent
+    );
+    $(parent).remove();
+  });
 });
