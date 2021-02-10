3744d4fa6ba012cc74a1a27b2227fdc228e17426
Shouvojit Sarker <sxsarker@agiledigital.com.au>
2021-02-10 13:09:00 +1100

    ADE-582: Update admin servlet's documentation and web resource

--- backend/src/main/java/au/com/agiledigital/idea_search/adminui/AdminServlet.java
+++ backend/src/main/java/au/com/agiledigital/idea_search/adminui/AdminServlet.java
@@ -12,9 +12,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.atlassian.webresource.api.assembler.PageBuilderService;
 
+/**
+ * Servlet to serve a admin configuration page on Configuration UI.
+ * See more at: https://developer.atlassian.com/server/confluence/adding-a-configuration-ui-for-your-plugin/
+ */
 public class AdminServlet extends HttpServlet
 {
   @ComponentImport
@@ -38,12 +41,14 @@ public class AdminServlet extends HttpServlet
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
   {
+    // require web resource to be able to use custom react
     pageBuilderService
       .assembler()
       .resources()
       .requireWebResource(
-        "au.com.agiledigital.idea_search:ideaSearch-macro-indexTable-macro-resource");
+        "au.com.agiledigital.idea_search:ideaAdminResource");
 
+    // verify that admin user is requesting the page
     String username = userManager.getRemoteUsername(request);
     if (username == null || !userManager.isSystemAdmin(username))
     {
@@ -51,6 +56,7 @@ public class AdminServlet extends HttpServlet
       return;
     }
 
+    // render admin configuration ui page
     response.setContentType("text/html;charset=utf-8");
     renderer.render("vm/admin.vm", response.getWriter());
   }
