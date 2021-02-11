d0cd9481662cd87eb4fe25bfc18c9b6ab6b873b0
Shouvojit Sarker <sxsarker@agiledigital.com.au>
2021-02-11 16:34:24 +1100

    ADE-613: Optimise imports and removed unused code

--- backend/src/main/java/au/com/agiledigital/idea_search/macros/StructuredData.java
+++ backend/src/main/java/au/com/agiledigital/idea_search/macros/StructuredData.java
@@ -1,47 +1,31 @@
 package au.com.agiledigital.idea_search.macros;
 
-import au.com.agiledigital.idea_search.service.DefaultFedexIdeaService;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
-import com.atlassian.confluence.setup.BootstrapManager;
 import com.atlassian.confluence.util.velocity.VelocityUtils;
-import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import com.atlassian.webresource.api.assembler.PageBuilderService;
 import com.google.gson.Gson;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.jsoup.Jsoup;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.commons.lang.WordUtils;
+import org.jsoup.Jsoup;
 
 public class StructuredData implements Macro {
-  private final PageBuilderService pageBuilderService;
   private Gson gson = new Gson();
-
-  @Autowired
-  public StructuredData(
-    @ComponentImport PageBuilderService pageBuilderService, @ComponentImport BootstrapManager bootstrapManager, DefaultFedexIdeaService fedexIdeaService) {
-    this.pageBuilderService = pageBuilderService;
-  }
 
   @Override
   public String execute(Map<String, String> map, String s, ConversionContext conversionContext)
     throws MacroExecutionException {
     Map<String, String> data = new LinkedHashMap<>();
+    // gets the page body data as mapped
     data =gson.fromJson(Jsoup.parse(s).body().text(), data.getClass());
-    Map<String, String> renderData = new LinkedHashMap<>();
 
+    Map<String, String> renderData = new LinkedHashMap<>();
+    // capitalises text values
     data.forEach( (r, t) -> {
       renderData.put(WordUtils.capitalize(r.replaceAll("[A-Z]", " $0")), t);
     });
-
-    pageBuilderService
-      .assembler()
-      .resources()
-      .requireWebResource(
-        "au.com.agiledigital.idea_search:ideaSearch-macro-structuredData-macro-resource");
 
     Map<String, Object> context = new HashMap<>();
     context.put("data", renderData);
